89ae395ae1d53c70f466dc4e45075911513efc8c
Boyoung Lee <genuss040822@gamil.com>
2024-05-27 20:48:13 +0900

    리액트 1강 과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+
+  const plusCount = () => {
+    setCount(count + 1);
+  };
+
+  const minusCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="layout">
+      <p>{count}</p>
+      <div>
+        <button onClick={plusCount}>➕</button>
+        <button onClick={minusCount}>➖</button>
+        <button onClick={resetCount}>reset</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
